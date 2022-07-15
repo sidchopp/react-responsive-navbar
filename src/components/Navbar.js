840a935be3b5116d7eaa8edcb1970d6cd2be8957
sidchopp <sidchopp@gmail.com>
2022-07-15 11:54:16 -0600

    Basic css included

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1> Hello World! </h1>
-      <p>I am a responsive Navbar</p>
-    </div>
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Responsive Navbar
+      </a>
+      <button className="hamburger">
+        <GiHamburgerMenu />
+      </button>
+      <div className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
