fce6d37613c105ca96ea383489cdb46eba31072d
sidchopp <sidchopp@gmail.com>
2022-07-15 11:24:15 -0600

    boiler plate removed,Navbar component initialized

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1> Hello World! </h1>
+      <p>I am a responsive Navbar</p>
+    </div>
+  );
+};
+
+export default Navbar;
